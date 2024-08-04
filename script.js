d0abdce6cee5c3811f9151f16d338e3ce2c546e7
Wisdom Jomo <jomowisdom212@gmail.com>
2024-08-04 11:57:51 +0100

    Submit button Added and every new input is now required

--- script.js
+++ script.js
@@ -3,12 +3,13 @@ const add = document.getElementById('add');
 add.addEventListener("click", (preD) => {
   preD.preventDefault();
   const emailInputs = form.querySelectorAll('.email-input');
-  if(emailInputs.length < 3) {
+  if(emailInputs.length < 5) {
     const newinput = document.createElement('input');
     newinput.type = 'email';
     newinput.placeholder = 'Enter Email Address';
     newinput.classList.add('email-input');
     newinput.value = 'demo@mail.com';
+    newinput.required = true;
     form.insertBefore(newinput, add);
   }
   else {
